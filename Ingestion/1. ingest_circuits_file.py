7269e129a0bf70038300582edb0751bbb4c8be40
UcheIgbokwe <UcheIgbokwe>
2024-01-09 21:20:55 +0000

    complete requirements for csv file

--- Ingestion/1. ingest_circuits_file.py
+++ Ingestion/1. ingest_circuits_file.py
@@ -30,7 +30,7 @@ circuits_schema = StructType(fields=[
 circuits_df = spark.read \
 .option("header", True) \
 .schema(circuits_schema) \
-.csv("dbfs:/mnt/formula1dluche/raw/circuits.csv")
+.csv("/mnt/formula1dluche/raw/circuits.csv")
 
 # COMMAND ----------
 
@@ -38,11 +38,60 @@ type(circuits_df)
 
 # COMMAND ----------
 
-display(circuits_df)
+# MAGIC %md
+# MAGIC ###### Step 2 - Select only required columns
 
 # COMMAND ----------
 
-circuits_df.printSchema()
+from pyspark.sql.functions import col
+
+# COMMAND ----------
+
+circuits_selected_df = circuits_df.select(col("circuitId"), col("circuitRef"), col("name"), col("location"), col("country"), col("lat"), col("lng"), col("alt"))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###### Step 3 - Rename the columns
+
+# COMMAND ----------
+
+circuits_rename_df = circuits_selected_df.withColumnRenamed("circuitId", "circuit_id") \
+.withColumnRenamed("circuitRef", "circuit_ref") \
+.withColumnRenamed("lat", "latitude") \
+.withColumnRenamed("lng", "longitude") \
+.withColumnRenamed("alt", "altitude")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###### Step 4 - Add new column
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp
+
+# COMMAND ----------
+
+circuits_final_df = circuits_rename_df.withColumn("ingestion_date", current_timestamp())
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###### Step 5 - Write data to DataLake in Parquet
+
+# COMMAND ----------
+
+circuits_final_df.write.mode("overwrite").parquet("/mnt/formula1dluche/processed/circuits")
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC ls /mnt/formula1dluche/processed/circuits
+
+# COMMAND ----------
+
+display(spark.read.parquet("/mnt/formula1dluche/processed/circuits"))
 
 # COMMAND ----------
 
