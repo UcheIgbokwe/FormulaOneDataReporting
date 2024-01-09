bc8c6c00f777a0541c3a25486a39aca8a70e2ec6
UcheIgbokwe <UcheIgbokwe>
2024-01-09 20:51:58 +0000

    ingest csv file

---
+++ Ingestion/1. ingest_circuits_file.py
@@ -0,0 +1,49 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC #### Ingest circuits.csv file
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###### Step 1 - Read the CSV file using the spark dataframe reader
+
+# COMMAND ----------
+
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DoubleType
+
+# COMMAND ----------
+
+circuits_schema = StructType(fields=[
+    StructField("circuitId", IntegerType(), False),
+    StructField("circuitRef", StringType(), True),
+    StructField("name", StringType(), True),
+    StructField("location", StringType(), True),
+    StructField("country", StringType(), True),
+    StructField("lat", DoubleType(), True),
+    StructField("lng", DoubleType(), True),
+    StructField("alt", IntegerType(), True),
+    StructField("url", StringType(), True)
+])
+
+# COMMAND ----------
+
+circuits_df = spark.read \
+.option("header", True) \
+.schema(circuits_schema) \
+.csv("dbfs:/mnt/formula1dluche/raw/circuits.csv")
+
+# COMMAND ----------
+
+type(circuits_df)
+
+# COMMAND ----------
+
+display(circuits_df)
+
+# COMMAND ----------
+
+circuits_df.printSchema()
+
+# COMMAND ----------
+
+
